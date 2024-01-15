8f3efe31ccb75080320d9837bd6090404a7b1fb7
hominjang <51106030+hominjang@users.noreply.github.com>
2024-01-15 22:43:48 +0900

    20240115
    LazyColum 만져보기

--- January/app/src/main/java/com/example/januarystudy/MainActivity.kt
+++ January/app/src/main/java/com/example/januarystudy/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.januarystudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.januarystudy.ui.theme.JanuaryStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +29,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+//                    GreetingCardView()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun GreetingCardView(order : Int){
+    Card (
+        Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+            )
+    {
+        Column {
+            Text(text = "Hello CardView $order")
+        }
+    }
+}
+
+@Composable
+fun GreetingLazyColumn(){
+    LazyColumn(){
+        items(30){
+            GreetingCardView(order = it)
         }
     }
 }
@@ -38,6 +69,7 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JanuaryStudyTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        GreetingLazyColumn()
     }
 }
